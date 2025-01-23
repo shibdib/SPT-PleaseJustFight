34d206403bff693b10a8054822e59d78658a9c1e
Bob S <shibdib@users.noreply.github.com>
2025-01-23 17:12:32 -0500

    1.0.2
    Scav and goon logic added

--- PleaseJustFight/Patches/SetHostilityPatch.cs
+++ PleaseJustFight/Patches/SetHostilityPatch.cs
@@ -18,21 +18,26 @@ namespace PleaseJustFight.Patches
         [PatchPostfix]
         private static void PatchPostfix(BotOwner __instance)
         {
-            SetPmcEnemies(__instance);
+            if (__instance.IsRole(WildSpawnType.pmcBEAR) || __instance.IsRole(WildSpawnType.pmcUSEC))
+            {
+                SetAllEnemies(__instance);
+            } else if (__instance.IsRole(WildSpawnType.assault) || __instance.IsRole(WildSpawnType.assaultGroup) || 
+                       __instance.IsRole(WildSpawnType.marksman))
+            {
+                SetPmcAsEnemies(__instance);
+            } else if (__instance.IsRole(WildSpawnType.bossKnight) || __instance.IsRole(WildSpawnType.followerBirdEye) || 
+                       __instance.IsRole(WildSpawnType.followerBigPipe))
+            {
+                SetPmcAsEnemies(__instance);
+            }
         }
 
-        private static void SetPmcEnemies(BotOwner newBot)
+        // Set pmc's as hostile towards all other bots with some boss exceptions
+        private static void SetAllEnemies(BotOwner newBot)
         {
-            // Only PMC's
-            if (!newBot.IsRole(WildSpawnType.pmcBEAR) && !newBot.IsRole(WildSpawnType.pmcUSEC))
-            {
-                return;
-            }
-            
             IEnumerable<IPlayer> humanPlayers = Singleton<GameWorld>.Instance.AllAlivePlayersList
                 .Where(p => !p.IsAI);
             
-            // Add all players to enemy list
             foreach (IPlayer humanPlayer in humanPlayers)
             {
                 newBot.BotsGroup.AddEnemy(humanPlayer, EBotEnemyCause.initial);
@@ -40,10 +45,8 @@ namespace PleaseJustFight.Patches
 
             IEnumerable<BotOwner> activatedBots = Singleton<IBotGame>.Instance.BotsController.Bots.BotOwners
                 .Where(b => b.BotState == EBotState.Active && b.Profile.Id != newBot.Profile.Id);
-
             foreach (BotOwner bot in activatedBots)
             {
-                // Special cases like santa, zyrachiy and btr
                 if (bot.IsRole(WildSpawnType.gifter) || bot.IsRole(WildSpawnType.shooterBTR) || 
                     bot.IsRole(WildSpawnType.bossZryachiy) || bot.IsRole(WildSpawnType.peacefullZryachiyEvent) 
                     || bot.IsRole(WildSpawnType.followerZryachiy))
@@ -55,5 +58,25 @@ namespace PleaseJustFight.Patches
                 newBot.BotsGroup.AddEnemy(bot, EBotEnemyCause.initial);
             }
         } 
+
+        // Set scavs and goons as hostile towards all pmc's
+        private static void SetPmcAsEnemies(BotOwner newBot)
+        {
+            IEnumerable<IPlayer> humanPlayers = Singleton<GameWorld>.Instance.AllAlivePlayersList
+                .Where(p => !p.IsAI);
+            
+            foreach (IPlayer humanPlayer in humanPlayers)
+            {
+                newBot.BotsGroup.AddEnemy(humanPlayer, EBotEnemyCause.initial);
+            }
+
+            IEnumerable<BotOwner> activatedBots = Singleton<IBotGame>.Instance.BotsController.Bots.BotOwners
+                .Where(b => b.BotState == EBotState.Active && (b.IsRole(WildSpawnType.pmcBEAR) || b.IsRole(WildSpawnType.pmcUSEC)));
+            foreach (BotOwner bot in activatedBots)
+            {
+                bot.BotsGroup.AddEnemy(newBot, EBotEnemyCause.initial);
+                newBot.BotsGroup.AddEnemy(bot, EBotEnemyCause.initial);
+            }
+        }
     }
 }
